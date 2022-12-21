ca8bfbacb62cd7716eba963fcf70482127417530
Edson Sales <edsonsales99@gmail.com>
2022-12-21 12:59:05 -0400

    Criado contexto para compartilhar informações

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Cabecalho from "components/Cabecalho";
 import Container from "components/Container";
 import Rodape from "components/Rodape";
+import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -9,12 +10,14 @@ function AppRoutes() {
     return (
         <BrowserRouter>
             <Cabecalho />
-                <Container>
+            <Container>
+                <FavoritosProvider>
                     <Routes>
                         <Route path="/" element={<Inicio />}> </Route>
                         <Route path="/favoritos" element={<Favoritos />}> </Route>
                     </Routes>
-                </Container>
+                </FavoritosProvider>
+            </Container>
             <Rodape />
         </BrowserRouter>
     )
